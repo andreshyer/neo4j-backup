f366c2467564b763a393f64da139dd6749c7ec15
Andres Hyer <hyerap@vcu.edu>
2021-08-01 14:58:36 -0400

    Undo dummy commits

--- example_import.py
+++ example_import.py
@@ -16,5 +16,3 @@ if __name__ == "__main__":
     input_yes = False
     importer = Importer(project_dir="data_dump", driver=driver, database=database, input_yes=input_yes)
     importer.import_data()
-
-    # This is a test lol
