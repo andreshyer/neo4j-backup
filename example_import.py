e9c75d2594ab882989a3397a5fdbafa7a805c236
Andres Hyer <hyerap@vcu.edu>
2022-10-23 15:09:16 -0400

    change database to neo4j as default

--- example_import.py
+++ example_import.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     trust = "TRUST_ALL_CERTIFICATES"
     driver = GraphDatabase.driver(uri, auth=(username, password), encrypted=encrypted, trust=trust)
 
-    database = "dev"
+    database = "neo4j"
 
     project_dir = "data_dump"
     input_yes = False
